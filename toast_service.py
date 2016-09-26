dbe01ea28da0e0d3e2cd98c1d88143d181c487e7
dinghao-bd <619015618@qq.com>
2016-09-26 23:19:02 +0800

    update

--- toast_service.py
+++ toast_service.py
@@ -11,17 +11,38 @@
 
 import multiprocessing
 import time
+import subprocess
+from uiautomator import device as d
 
 
-class ToastService(multiprocessing.Process):
-    """
-    执行命令：adb shell uiautomator events 抓取accessbility 的内容，然后分析内容，取得toast
-    """
-    def __init__(self):
-        super(ToastService).__init__(self)
 
-    def parse_content(self, content):
-        pass
 
-    def run(self):
-        pass
+
+# class ToastService(multiprocessing.Process):
+#     """
+#     执行命令：adb shell uiautomator events 抓取accessbility 的内容，然后分析内容，取得toast
+#     """
+#
+#     #
+#     # def __init__(self):
+#     #     super(ToastService).__init__(self)
+#
+#     def __init__(self, group=None, target=None, name=None, args=(), kwargs={}):
+#         super(ToastService, self).__init__(group=None, target=None, name=None, args=(), kwargs={})
+#         cmd = "adb shell uiautomator events"
+#
+#         self.child = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+#         self.child.wait()
+#         print self.child.stdout.read()
+#
+#     def parse_content(self):
+#         # with open('events.txt', 'r') as f:
+#         #     print f.readline()
+#         pass
+#
+#
+# if __name__ == '__main__':
+#     toast_service = ToastService()
+#     toast_service.parse_content()
+child1 = subprocess.Popen('adb shell uiautomator events', shell=True, stdout=subprocess.PIPE)
+print child1.stdout.read()
